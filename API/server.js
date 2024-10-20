2c6cd509f404d3de068784e77a2f6001a32493bb
LuciferM121 <rojasrodriguezmartinomar@gmail.com>
2024-10-20 15:23:01 -0600

    example crud

--- API/server.js
+++ API/server.js
@@ -2,18 +2,28 @@ const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
+const productos = [
+    { id: 1, foto: '/Totita.jpg', nombre: 'Platillo 1', costo: 50.50, descripcion: 'Descripción 1', categoria: 'Carnes' },
+    { id: 2, foto: '/limonada.jpg', nombre: 'Bebida 3', costo: 70.00, descripcion: 'Descripción 2', categoria: 'Ensaladas' },
+    { id: 3, foto: '/monster.jpg', nombre: 'Bebida 1', costo: 30.00, descripcion: 'Descripción 3', categoria: 'Postres' },
+    { id: 4, foto: '/jamaica.jpg', nombre: 'Bebida 2', costo: 40.00, descripcion: 'Descripción 4', categoria: 'Bebidas' },
+    // Más productos...
+];
 
 app.use(morgan('dev'))
+app.use(express.json())
 
 app.listen(3000)
 console.log(`Server on port ${3000}`)
 
 app.get('/productos', (req, res) => {
-    res.send('obteniendo productos')
+    res.json(productos)
 })
 
 app.post('/productos', (req, res) => {
-    res.send('creando productos')
+    const nuevoProducto = { ...req.body, id: productos.length + 1 }
+    productos.push(nuevoProducto)
+    res.send(nuevoProducto)
 })
 
 app.put('/productos', (req, res) => {
@@ -25,5 +35,13 @@ app.delete('/productos', (req, res) => {
 })
 
 app.get('/productos/:id', (req, res) => {
-    res.send('obteniendo producto')
+    console.log(req.params.id)
+    const productoEncontrado =  productos.find(
+        (producto) => producto.id ===  parseInt(req.params.id)
+    )
+    if(!productoEncontrado) return res.status(404).json({
+        message: "Product not found"
+    })
+    console.log(productoEncontrado)
+    res.json()
 })
