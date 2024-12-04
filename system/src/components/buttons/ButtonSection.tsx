72d053dfcea61d22d0cd85a1de5ff4522b1f0f67
Evanson07 <evan.apj37@gmail.com>
2024-12-03 23:27:56 -0600

    RegisterButton
    Dark mode and Register button

--- system/src/components/buttons/ButtonSection.tsx
+++ system/src/components/buttons/ButtonSection.tsx
@@ -9,12 +9,12 @@ interface ButtonSectionProps {
 const ButtonSection = ({ text, icon, onClick }: ButtonSectionProps) => {
     return (
         <div className='group' onClick={onClick}>
-            <div className=' cursor-pointer border h-80 w-96 md:w-full bg-zinc-950 rounded-lg flex flex-col items-center justify-center gap-10 p-5 text-zinc-200 transition-all duration-300 ease-in-out hover:shadow-[0_10px_20px_rgba(0,0,0,0.7)] hover:-translate-y-2 hover:bg-zinc-900'>
+            <div className=' cursor-pointer border h-80 w-96 md:w-full dark:bg-zinc-950 bg-zinc-200 rounded-lg flex flex-col items-center justify-center gap-10 p-5 dark:text-zinc-200 text-black transition-all duration-300 ease-in-out hover:shadow-[0_10px_20px_rgba(0,0,0,0.7)] hover:-translate-y-2 dark:hover:bg-zinc-200 hover:bg-zinc-900 dark:hover:text-black'>
                 <div className='flex flex-col items-center justify-center gap-10 transition-all duration-300 ease-in-out group-hover:scale-105'>
-                    <div className='text-8xl transition-all duration-300 ease-in-out group-hover:text-zinc-100'>
+                    <div className='text-8xl transition-all duration-300 ease-in-out dark:text-zinc-100 text-black group-hover:text-zinc-100 dark:group-hover:text-black'>
                         {icon}
                     </div>
-                    <h1 className='font-bold text-center text-3xl transition-all duration-300 ease-in-out group-hover:text-zinc-100'>{text}</h1>
+                    <h1 className='font-bold text-center text-3xl transition-all duration-300 ease-in-out text-black group-hover:text-zinc-100 dark:text-zinc-100 dark:group-hover:text-black'>{text}</h1>
                 </div>
             </div>
         </div>
