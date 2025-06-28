d2f03804b715be9ef5fbeaf6858cd76c39a7debe
LuciferM12 <rojasrodriguezmartinomar@gmail.com>
2025-06-28 14:36:43 -0600

    test: test prettier

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,13 @@
-'use client'
+'use client';
 
-import { useState } from 'react'
+import { useState } from 'react';
+
+const test = 'test';
 
 export default function Home() {
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-gradient-to-br from-purple-100 to-blue-100">
       <h1 className="text-3xl font-bold mb-6 text-gray-800">Galería de Imágenes</h1>
     </main>
-  )
+  );
 }
